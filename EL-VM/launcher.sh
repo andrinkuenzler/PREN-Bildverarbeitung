0fe179b4f34e88669c5e0dbd51cb926663ec470c
akunzler <andrin.kuenzler@stud.hslu.ch>
2023-04-14 08:57:46 +0200

    Added launcher.sh

---
+++ EL-VM/launcher.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# launcher.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+sudo python3 /home/localadmin/PREN-Bildverarbeitung/EL-VM/main-vm.py
