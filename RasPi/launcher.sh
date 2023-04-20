f9b560050671ce5157c6b7badc69c270b0336ebc
akunzler <andrin.kuenzler@stud.hslu.ch>
2023-04-20 11:17:20 +0200

    Refactoring

--- RasPi/launcher.sh
+++ RasPi/launcher.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # launcher.sh
 # navigate to home directory, then to this directory, then execute python script, then back home
-sudo python3 /home/pi/PREN-Bildverarbeitung/RasPi/main-pi.py
+sleep 30
+python3 /home/pi/PREN-Bildverarbeitung/RasPi/main-pi.py
